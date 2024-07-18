83e15e0b0ccdea129b974b201f4b96de86a2dca7
throskam <throskam@gmail.com>
2024-07-18 09:26:02 +0200

    feat(nvim): change dap UI layout

--- nvim/.config/nvim/lua/throskam/plugins/nvim-dap-ui.lua
+++ nvim/.config/nvim/lua/throskam/plugins/nvim-dap-ui.lua
@@ -9,55 +9,26 @@ return {
 	config = function()
 		local dapui = require("dapui")
 
-		dapui.setup({
-			layouts = {
-				{
-					elements = {
-						{
-							id = "scopes",
-							size = 1,
-						},
-					},
-					position = "bottom",
-					size = 10,
-				},
-				{
-					elements = {
-						{
-							id = "watches",
-							size = 1,
-						},
-					},
-					position = "bottom",
-					size = 10,
-				},
-				{
-					elements = {
-						{
-							id = "breakpoints",
-							size = 1,
-						},
-					},
-					position = "bottom",
-					size = 10,
-				},
-			},
-		})
+		dapui.setup()
 
-		vim.keymap.set({ "n", "v" }, "<Leader>dK", function()
+		vim.keymap.set({ "n", "v" }, "<Leader>de", function()
 			dapui.eval(nil, { enter = true })
 		end, { desc = "DAP UI eval" })
 
-		vim.keymap.set("n", "<Leader>df", function()
-			dapui.toggle(1)
-		end, { desc = "DAP UI toggle scopes" })
+		vim.keymap.set("n", "<Leader>ds", function()
+			dapui.float_element("scopes", { enter = true })
+		end, { desc = "DAP UI scopes" })
 
 		vim.keymap.set("n", "<Leader>dw", function()
-			dapui.toggle(2)
-		end, { desc = "DAP UI toggle watches" })
+			dapui.float_element("watches", { enter = true })
+		end, { desc = "DAP UI watches" })
 
 		vim.keymap.set("n", "<Leader>db", function()
-			dapui.toggle(3)
-		end, { desc = "DAP UI toggle breakpoints" })
+			dapui.float_element("breakpoints", { enter = true })
+		end, { desc = "DAP UI breakpoints" })
+
+		vim.keymap.set("n", "<Leader>df", function()
+			dapui.float_element("frames", { enter = true })
+		end, { desc = "DAP UI frames" })
 	end,
 }
