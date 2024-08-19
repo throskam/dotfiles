46acca437540e2d86041e80d8739358003726879
throskam <throskam@gmail.com>
2024-08-19 12:07:10 +0200

    feat(nvim): update live-grep-args mappings

--- nvim/.config/nvim/lua/throskam/plugins/telescope-nvim.lua
+++ nvim/.config/nvim/lua/throskam/plugins/telescope-nvim.lua
@@ -41,8 +41,7 @@ return {
 				live_grep_args = {
 					mappings = {
 						i = {
-							["<C-k>"] = lga_actions.quote_prompt(),
-							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+							["<C-y>"] = lga_actions.quote_prompt(),
 							["<C-space>"] = actions.to_fuzzy_refine,
 						},
 					},
