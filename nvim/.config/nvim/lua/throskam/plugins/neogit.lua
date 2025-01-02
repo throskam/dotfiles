c2d39fd3cb884280f343bdfdfea1ef71d4deca22
throskam <throskam@gmail.com>
2025-01-02 23:03:34 +0100

    feat(nvim): add Neogit mapping

--- nvim/.config/nvim/lua/throskam/plugins/neogit.lua
+++ nvim/.config/nvim/lua/throskam/plugins/neogit.lua
@@ -1,11 +1,15 @@
 -- Git integration
 return {
 	"NeogitOrg/neogit",
-	cmd = "Neogit",
+	keys = { "<leader>n" },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
-	config = true,
+	config = function()
+		require("neogit").setup({})
+
+		vim.keymap.set("n", "<leader>n", "<cmd>Neogit<cr>", { desc = "Open Neogit" })
+	end,
 }
