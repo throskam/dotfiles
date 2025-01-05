9469d2b65898cb5d728cc86bda22abc70b177f8d
throskam <throskam@gmail.com>
2025-01-06 00:04:57 +0100

    feat(nvim): add signature help suggestion

--- nvim/.config/nvim/lua/throskam/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/throskam/plugins/nvim-cmp.lua
@@ -3,11 +3,12 @@ return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-path",
 		"saadparwaiz1/cmp_luasnip",
 		"L3MON4D3/LuaSnip",
 		"onsails/lspkind.nvim",
@@ -51,7 +52,7 @@ return {
 							item.kind = icon
 							item.kind_hl_group = hl_group
 						end
-					else
+					elseif entry.source.name ~= "nvim_lsp_signature_help" then
 						item.menu = "[" .. item.menu .. "?]"
 					end
 
@@ -77,8 +78,8 @@ return {
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
-
 				{ name = "path" },
+				{ name = "nvim_lsp_signature_help" },
 			}, {
 				{ name = "buffer" },
 			}),
