aa09a89666ecde1116110bddc9ed3d9e2c3589d2
throskam <throskam@gmail.com>
2025-01-24 21:36:54 +0100

    feat(nvim): add LSP document highlight mappings

--- nvim/.config/nvim/lua/throskam/mappings.lua
+++ nvim/.config/nvim/lua/throskam/mappings.lua
@@ -5,3 +5,11 @@ vim.keymap.set("n", "<leader>c", [[:close<cr>]], { silent = true, desc = "Save" 
 vim.keymap.set("n", "<leader>q", [[:q<cr>]], { silent = true, desc = "Quit" })
 vim.keymap.set("n", "<leader>Q", [[:q!<cr>]], { silent = true, desc = "Force quit" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+
+vim.keymap.set("n", "<leader><", function()
+	vim.lsp.buf.document_highlight()
+end, { silent = true, desc = "Remove search highlights" })
+
+vim.keymap.set("n", "<leader>>", function()
+	vim.lsp.buf.clear_references()
+end, { silent = true, desc = "Remove search highlights" })
