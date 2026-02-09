6e9776a8736c0581581d70bb935acc091cde88a8
throskam <throskam@gmail.com>
2026-02-09 22:28:47 +0100

    fix(nvim): revert treesitter to master

--- nvim/.config/nvim/lua/throskam/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/throskam/plugins/nvim-treesitter.lua
@@ -1,9 +1,12 @@
 -- Better syntax tree
 return {
 	"nvim-treesitter/nvim-treesitter",
+	-- The new main branch as breaking changes
+	branch = "master",
 	-- DO NOT LAZY LOAD TREE SITTER
 	-- https://github.com/nvim-treesitter/nvim-treesitter/issues/5896
 	-- https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
