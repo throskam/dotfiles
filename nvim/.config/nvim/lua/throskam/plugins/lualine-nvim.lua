3a1939cc7f92a2c623fe54aa5b085f7c782257ec
throskam <throskam@gmail.com>
2026-03-29 12:12:41 +0200

    chore(nvim): remove unnecessary theme option in lualina

--- nvim/.config/nvim/lua/throskam/plugins/lualine-nvim.lua
+++ nvim/.config/nvim/lua/throskam/plugins/lualine-nvim.lua
@@ -4,9 +4,6 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
-			options = {
-				theme = "catppuccin",
-			},
 			sections = {
 				lualine_c = {
 					{
